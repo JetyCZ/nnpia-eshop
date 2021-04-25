219c5cc79601f1fd6dd3b23c4cc015f29575b00f
pavel.jetensky@seznam.cz <pavel.jetensky@seznam.cz>
2021-04-25 16:57:00 +0200

    Adding support for running Selenium in Chrome headless in CircleCI

--- src/test/java/cz/upce/eshop/ui/ProductUITest.java
+++ src/test/java/cz/upce/eshop/ui/ProductUITest.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import java.io.File;
+import java.net.MalformedURLException;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(Creator.class)
@@ -34,13 +36,38 @@ public class ProductUITest {
 
     @BeforeAll
     public static void setupWebdriverChromeDriver() {
-        String chromedriverPath =  ProductUITest.class.getResource("/chromedriver.exe").getFile();
+
+        String chromedriverPath = ProductUITest.class.getResource("/chromedriver.exe").getFile();
         System.setProperty("webdriver.chrome.driver", chromedriverPath);
+
+        String circleCIChromedriverPath = "/usr/local/bin/chromedriver";
+        if (new File(circleCIChromedriverPath).exists()) {
+            System.setProperty("webdriver.chrome.driver", circleCIChromedriverPath);
+        }
     }
 
     @BeforeEach
-    public void setup() {
-        driver = new ChromeDriver();
+    public void setup() throws MalformedURLException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
+        driver = new ChromeDriver(chromeOptions);
+
+        /*
+
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome()
+            );
+
+         */
+
+/*
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "c:\\Users\\zuzka\\IdeaProjects\\nnpia-eshop-clone\\nnpia-eshop\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe");
+        caps.setCapability("takesScreenshot", true);
+        PhantomJSDriver driver = new PhantomJSDriver(caps);
+*/
         productRepository.deleteAll();
 
     }
@@ -70,7 +97,7 @@ public class ProductUITest {
     private void singleProductAddTest() {
         driver.get("http://localhost:" + localServerPort + "/product-form");
         driver.findElement(By.id("productName")).sendKeys("nůžky");
-        driver.findElement(By.id("image")).sendKeys("c:\\Users\\zuzka\\Pictures\\stažený soubor.png" );
+        driver.findElement(By.id("image")).sendKeys("c:\\Users\\zuzka\\Pictures\\stažený soubor.png");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
 
         Assert.assertEquals(1, driver.findElements(By.xpath("//h2[text()='Product list']")).size());
